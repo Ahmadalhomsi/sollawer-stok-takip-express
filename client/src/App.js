b018d92cd01a559cd443ad225c89e2f12c6cc951
Ahmad <tarikuka.aa@gmail.com>
2024-07-03 22:30:35 +0300

    Cors & API requests

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,34 @@
-rfc
+import React from 'react'
+
+
+
+export default function App() {
+
+
+
+    const handleClick = () => {
+        fetch('http://localhost:5000/api') // Replace '/api/data' with your server endpoint
+            .then(response => response.json())
+            .then(data => {
+                // Handle the fetched data here
+                console.log('Data from serverXX:');
+                console.log(data);
+            })
+            .catch(error => {
+                // Handle any errors that occur during the fetch request
+                console.log('Error fetching data from serverZZ:');
+                console.error(error);
+            });
+    }
+
+
+
+    return (
+        <div>App
+            <input type="text" placeholder='Message' />
+            <button onClick={handleClick}>
+                Click Me!
+            </button>
+        </div>
+    )
+}
--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors')
+
+app.use(cors())
+
 // GET route
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     //res.send('Hello, world!');
     res.json({ "users": ["userOne", "UserTwo"] });
 });
